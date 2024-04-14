7c42a8eb825bfc54797cc8d3e93cd90f4493422d
Dima S. <coddeys@gmail.com>
2024-04-14 23:28:46 +0200

    Upgrade aiken

--- nativetokens/testnet/mint.sh
+++ nativetokens/testnet/mint.sh
@@ -1,13 +1,14 @@
-utxoin1="7f1409ec3e5861a6371a3b9a10117538177d11d3dbba056254d12b4b14699145#4"
+utxoin1="0d9cc680b5e8a6b360a9d36a2406fa238e9bac185bf4d973d331efbcf761fc8f#1"
 policyid=$(cat ak_eaCoins.pid)
 output="2200000000"
 tokenamount="100"
 tokenname=$(echo -n "B98aikenToken" | xxd -ps | tr -d '\n')
-mintingScript="./compiled/ak_eaCoins.uplc"
-collateral="e623894e267cf3d4553327108d566d61b01298a1b56363b260ffb5e768578913#0"
-signerPKH="2070f8488dd696b78a5f23e38d273550e43660526c4b19cba733b488"
-ownerPKH="d40e3e791bead1fd8c003f1227906b66a2a1c04e59d663798017db71"
+mintingScript="../artifacts/eacoins.eaCoins.uplc"
+collateral="bcf593a50a1b36aaeda2e55aaac8a6f6452db05caba72ec87bcf89080146a670#1"
+signerPKH="8b225ceddb05738d7a53bd130136e187a6f0baa4d219161fed4f2ac0"
+ownerPKH="6efff2244d9f67a1a383db4a198a10c647408767cdcb0a44845ce15a"
 notneeded="--invalid-hereafter 10962786"
+PREVIEW="--testnet-magic 2"
 
 cardano-cli transaction build \
   --babbage-era \
@@ -16,8 +17,8 @@ cardano-cli transaction build \
   --required-signer-hash $ownerPKH \
   --required-signer-hash $signerPKH \
   --tx-in-collateral $collateral \
-  --tx-out $nami3+$output+"$tokenamount $policyid.$tokenname" \
-  --change-address $nami3 \
+  --tx-out $nami+$output+"$tokenamount $policyid.$tokenname" \
+  --change-address $nami \
   --mint "$tokenamount $policyid.$tokenname" \
   --mint-script-file $mintingScript \
   --mint-redeemer-file ./values/redeemer.json \
@@ -25,11 +26,11 @@ cardano-cli transaction build \
 
 cardano-cli transaction sign \
     --tx-body-file mintTx.body \
-    --signing-key-file ../../../Wallets/Adr12.skey \
-    --signing-key-file ../../../Wallets/Adr11.skey \
+    --signing-key-file ../../wallets/person1.skey \
+    --signing-key-file ../../wallets/person1.skey \
     $PREVIEW \
     --out-file mintTx.signed
 
- cardano-cli transaction submit \
-    $PREVIEW \
-    --tx-file mintTx.signed
+ # cardano-cli transaction submit \
+ #    $PREVIEW \
+ #    --tx-file mintTx.signed
